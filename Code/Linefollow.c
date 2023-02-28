f5cc5b5103fcf9f50fbe63ded34808cc179365c1
Flits <kasperjnssen@gmail.com>
2023-03-01 00:10:22 +0100

    Primitive code but took me way too long tbh

---
+++ Code/Linefollow.c
@@ -0,0 +1,106 @@
+#include <pololu/3pi.h>
+#include <avr/pgmspace.h>
+
+//the startup screen
+const char startup_line1[] PROGMEM = "RobotRun";
+const char startup_line2[] PROGMEM = "KARBONKEL";
+const char startup_line3[] PROGMEM = "ARBONKEL";
+const char StartupMelody[] PROGMEM = "T180 O5 MS L8 EERERCE4 L4 GR<GR ";
+
+//Until line 80 everything is for testing purposes
+
+const char levels[] PROGMEM = {
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b00000,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111
+};
+
+// This function loads custom characters into the LCD.  Up to 8
+// characters can be loaded; we use them for 7 levels of a bar graph.
+void load_custom_characters()
+{
+	lcd_load_custom_character(levels+0,0); // no offset, e.g. one bar
+	lcd_load_custom_character(levels+1,1); // two bars
+	lcd_load_custom_character(levels+2,2); // etc...
+	lcd_load_custom_character(levels+3,3);
+	lcd_load_custom_character(levels+4,4);
+	lcd_load_custom_character(levels+5,5);
+	lcd_load_custom_character(levels+6,6);
+	clear(); // the LCD must be cleared for the characters to take effect
+}
+
+// This function displays the sensor readings using a bar graph.
+void display_readings(const unsigned int *calibrated_values)
+{
+	unsigned char i;
+
+	for(i=0;i<5;i++) {
+		// Initialize the array of characters that we will use for the
+		// graph.  Using the space, an extra copy of the one-bar
+		// character, and character 255 (a full black box), we get 10
+		// characters in the array.
+		const char display_characters[10] = {' ',0,0,1,2,3,4,5,6,255};
+
+		// The variable c will have values from 0 to 9, since
+		// calibrated values are in the range of 0 to 1000, and
+		// 1000/101 is 9 with integer math.
+		char c = display_characters[calibrated_values[i]/101];
+
+		// Display the bar graph character.
+		print_character(c);
+	}
+}
+//end test code
+
+
+void initRobot()
+{
+	unsigned int counter;
+	unsigned int sensors[5]; // the other 2 will be done differently and are currently out of the scope of this code
+	
+	pololu_3pi_init(2000);
+	load_custom_characters();
+	
+	//play welcome line
+	print_from_program_space(startup_line1);
+	lcd_goto_xy(0,1);
+	print_from_program_space(startup_line2);
+	play_from_program_space(StartupMelody);
+	delay_ms(2000);
+	
+	//calibration
+	for(int calibrationCounter=0; calibrationCounter<180; calibrationCounter++)
+	{
+		calibrate_line_sensors(IR_EMITTERS_ON);//turns on all IR emitters
+	}
+}
+
+// This is the main function, where the code starts.  All C programs
+// must have a main() function defined somewhere.
+int main()
+{
+	initRobot();
+	
+	while(1){
+		set_motors(0,0);
+	}
+	
+}
+
+// Local Variables: **
+// mode: C **
+// c-basic-offset: 4 **
+// tab-width: 4 **
+// indent-tabs-mode: t **
+// end: **
