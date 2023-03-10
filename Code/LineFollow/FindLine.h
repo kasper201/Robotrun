034386af80b03719a542ac455bbf7b68adbd66e2
Flits <kasperjnssen@gmail.com>
2023-03-10 11:51:13 +0100

    Update FindLine.h
    fixed it

--- Code/LineFollow/FindLine.h
+++ Code/LineFollow/FindLine.h
@@ -3,7 +3,7 @@
 
 void initRobot();
 void startDrivingAfter();
-void followLine(&typeOfCrossing, inMaze);
-void turn(turnTo);
+void followLine(int *typeOfCrossing, int inMaze);
+void turn(int turnTo);
 
 #endif // FINDLINE
