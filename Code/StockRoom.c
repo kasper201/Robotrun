5e662881de9c63aaa6589c527b9325435a3dcd7d
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-03-17 14:48:03 +0100

    Update StockRoom.c

--- Code/StockRoom.c
+++ Code/StockRoom.c
@@ -2,23 +2,101 @@
 #include <avr/pgmspace.h>
 #include "StockRoom.h"
 
+int packageAmount;
+int crossing;
 enum direction{minX, plusX, minY, plusY};
-struct coords{int Xc, Yc, Xp, Yp, Xarray[30], Yarray[30]};
-	
-void stockroomStart()
+enum direction facing;
+struct coords{int Xcurrent, Ycurrent, Xpackage, Ypackage;};
+struct coords p1;
+
+void stockroomRoutine()
 {
-	enum direction facing = minX;    //driveTill(Xp (all) = Xc)
-	
+	if(packageAmount != 0)//there are still orders to be completed
+	{
+		switch(facing) //turn facing minus X
+		{
+			case minY:
+			turn(1);
+			facing = minX;
+			break;
+			case plusX:
+			turn(2);
+			facing = minX;
+			break;
+			case plusY:
+			turn(3);
+			facing = minX;
+			break;
+		}
+		while(p1.Xcurrent<p1.Xpackage) //drive until the X coords is reached
+		{
+			followLine(&crossing, 0);
+			if(crossing != 0)
+			{
+				turn(0);
+			}
+		}
+		findPackageY();	//get all the packages on this X
+	}
+	else if(packageAmount <= 0) //there are no orders left
+	{
+		TurnBack();//drive back to the maze
+	}
 }
 
-void findPackage()
+void findPackageY()
 {
- 
+	
 }
 
 void passingToCharge()
 {
 	turn(1);
-	
+	while(crossing != 1)
+	{
+		followLine(&crossing, 0);
+		if(crossing != 0)
+		{
+			turn(0);
+		}
+	}
 	turn(1);
+	followLine(&crossing, 0);
+}
+
+void TurnBack()
+{
+	switch(facing)
+	{
+		case plusX:
+		turn(1);
+		facing = minY;
+		break;
+		case plusY:
+		turn(2);
+		facing = minY;
+		break;
+		case minX:
+		turn(3);
+		facing = minY;
+		break;
+	}
+	while(crossing != 1 && crossing != 5)
+	{
+		followLine(&crossing, 0);
+		if(crossing != 0)
+		{
+			turn(0);
+		}
+	}
+	turn(3);
+	facing = plusX;
+	while(*mazeBorder != true)
+	{
+		followLine(&crossing, 0);
+		if(crossing != 0)
+		{
+			turn(0);
+		}
+	}
 }
