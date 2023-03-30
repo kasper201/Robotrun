61c288bbf7e75d11b7dd610365a99c94d031da09
Flits <kasperjnssen@gmail.com>
2023-03-31 00:00:50 +0200

    Update SolveMaze.c
    Massive changes

--- Code/SolveMaze/SolveMaze.c
+++ Code/SolveMaze/SolveMaze.c
@@ -8,118 +8,110 @@
  *  Author: Kaspe
  */
 
+/*
+ * Part of this code was derived but not copied from Pololu at https://www.pololu.com/docs/0J21/8.e
+ * Though this isn't a copy or just plain rewritten in different variable names large parts do look alike
+ */
+
 #include <pololu/3pi.h>
 #include "FindLine.h"
 
-void routeKnown(int sizeArray, int turnArray[], int **state)
+int arrayToStockroom[64];
+int arrayFromStockroom[64];
+
+void routeKnown()
 {
-	int inMaze = 1;
-	int resetCrossing = 0;
-	if(*(*(state)) == 0)//if stockStart == 0 robot will go from start to stockroom otherwise if stockStart == 1 the robot will go from stockroom to start
-	{
-		for(int i = 0; i <= sizeArray; i++)
-		{
-			followLine(&resetCrossing, inMaze);
-			turn(turnArray[i]);
-			set_motors(50,50);
-			delay_ms(50);
-			**state = 1;
-		}
-	}
-	
-	else
-	{
-		for(int i = sizeArray; i >= 0; i--)
-		{
-			followLine(&resetCrossing, inMaze);
-			turn(turnArray[i]);
-			set_motors(50,50);
-			delay_ms(50);
-			**state = 0;
-		}
-	}
+
 }
 
-void solveMaze(int *irouteKnown, int sizeArray, int (*turnArray)[sizeArray], int *state) //set when implementing the code the state to Stockroom((5,0, facing -x) do this together with Mik
+void simplify(int crossing, int *pathLength)
 {
-	int crossing;
-	int inMaze = 1;
-	int foundEnd = 0;
-	int whatkindofcrossing[64];
-	int deadEndsFound = 0;
-	if(*irouteKnown == 1)//if route known just drive like the old array
+	int turnValue = 0;
+	int i = 0;
+	int totalTurn = 0;
+	if(pathLength < 3 || arrayToStockroom[pathLength-2] != 2)
+		return;
+	
+	for(i = 0; i <= 3; i++)
 	{
-		routeKnown(sizeArray, *turnArray, &state);
+		switch(arrayToStockroom[pathLength-i])
+		{
+			case 1:
+			totalTurn += 1;
+			break;
+			
+			case 2:
+			totalTurn += 2;
+			break;
+			
+			case 3:
+			totalTurn += 3;
+			break;
+		}
+	}
+	totalTurn %= 4;
+	
+	switch(totalTurn)
+	{
+		case 0:
+		arrayToStockroom[i-3] = 0;
+		break;
+		
+		case 1:
+		arrayToStockroom[i-3] = 1;
+		break;
+		
+		case 2:
+		arrayToStockroom[i-3] = 2;
+		break;
+		
+		case 3;
+		arrayToStockroom[i-3] = 4;
+		break;
+	}
+	*pathLength -= 2;
+}
+
+void solveMaze() //set when implementing the code the state to Stockroom((5,0, facing -x) do this together with Mik
+{
+	static int irouteKnown = 0;
+	
+	if(irouteKnown = 0)
+	{
+		routeKnown();
 	}
 	else
 	{
-		int turnValue = 1;
 		int i = 0;
-		while(foundEnd != 1) //go on untill end of maze found
+		while(1)
 		{
-			crossing = 0;
-			followLine(&crossing, inMaze);//drive till crossing or dead end found
-			whatkindofcrossing[i] = crossing; //save what type of crossing
+			int crossing = 0;
+			int inMaze = 1;
+			followLine(&crossing, inMaze);
 			if(crossing == 7)
 			{
-				foundEnd = 1;
+				set_motors(75,75);
+				delay_ms(100);
+				break;
 			}
-			else if(crossing == 2) // if able to go left and straight go straight
+			else if(crossing == 2)
 			{
 				turn(0);
-				*turnArray[i] = 0;
+				arrayToStockroom[i] = 0;
 			}
-			else if(crossing == 99) // if end of line turn around
+			else if(crossing == 99)
 			{
 				turn(2);
-				if(turnValue == 0) // if straight was wrong go left
-				{
-					turnValue = 3;
-				}
-				else if(turnValue == 1) // if right was wrong go straight
-				{
-					turnValue = 0;
-				}
-				else
-				{
-					i--;
-					if((*turnArray[i-1] == 1) && whatkindofcrossing[i-1] != 1) // if last turn was to go to right thats wrong so go straight unless unable to go straight (seen from if its a new route)
-					{
-						turnValue = 0;
-					}
-					else if((*turnArray[i-1] == 1) && whatkindofcrossing[i-1] == 1)
-					{
-						turnValue = 3;
-					}
-					else if((*turnArray[i-1] == 0) && whatkindofcrossing[i-1] != 2) // if last turn was to go straight go left unless unable to go left (seen from if you restart the route
-					{
-						turnValue = 3;
-					}
-					else 
-					{
-						// won't work.... I probably could make it work indefinetely but that would require some more clever thinking
-					}
-				}
-				i--;
-				continue; //go back to the top to take next turn
+				arrayToStockroom[i] = 2;
 			}
-			else //normal crossing and able to go right so go right
+			else
 			{
 				turn(1);
-				*turnArray[i] = turnValue; //save turn value. this value changes based on if its been at a dead end
+				arrayToStockroom[i] = 1;
 			}
 			i++;
-			deadEndsFound++;
+			simplify(crossing, &i);
 		}
-		if(deadEndsFound > 0)
-		{
-			*irouteKnown = 0;
-		}
-		else
-		{
-			*irouteKnown = 1;
-		}
-		*state = 1;
 	}
 }
 
@@ -127,12 +119,8 @@ int main()
 { 
 	initRobot();
 	
-	int array[] = {1,3,1,2,2};//will become empty in actual code, this is just for testing purposes
-	int sizeArray = sizeof(array)/sizeof(int);
-	int state = 0;
-	int irouteKnown = 0;
 	while(1)
 	{
-		solveMaze(&irouteKnown, sizeArray, &array, &state);
+		solveMaze();
 	}
 }
