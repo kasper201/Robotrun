a224bf1a60f81ca1c4b1dc749aefdd3234e50754
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-04 20:13:16 +0200

    Update charge.c

--- Code/charging/charge.c
+++ Code/charging/charge.c
@@ -1,1 +1,58 @@
+void passingToCharge()
+{
+	int crossing=0;
+	followLine(&crossing, 0);
+	turn(1);
+	while(crossing!=1)
+	{
+		crossing=0;
+		followLine(&crossing, 0);
+		if(crossing!=0)
+		{
+			turn(0);
+		}
+	}
+	turn(1);
+	set_motors(50, 50);
+	delay_ms(200);
+}
 
+passingToCharge2()
+{
+	int crossing=0;
+	followLine(&crossing, 0);
+	turn(3);
+	while(crossing!=1)
+	{
+		crossing=0;
+		followLine(&crossing, 0);
+		if(crossing!=0)
+		{
+			turn(0);
+		}
+	}
+	turn(3);
+	while(crossing!=7)
+	{
+		crossing=0;
+		followLine(&crossing, 0);
+	}
+}
+
+void batteryLevel()
+{
+	unsigned int read_battery_millivolts_3pi()
+	{
+		return readAverage(6,10)*5000L*3/2/1023;
+	}
+}
+
+void timeToCharge()
+{
+	clear();
+	print("FLOEFS");
+	lcd_goto_xy(0,1);
+	print("SLEEPS");
+	play("o5 c#" );
+	delay_ms(10000);
+}
