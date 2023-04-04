f0b3e13ee6204da6c668d4410869ac1175d9915c
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-04 20:10:59 +0200

    Update stockroom.h

--- Code/stockroom/stockroom.h
+++ Code/stockroom/stockroom.h
@@ -7,10 +7,8 @@ enum direction facing;
 struct coords{int Xcurrent, Ycurrent, Xpackage;};
 struct coords p1;
 
-struct orders{int Xorders[32], Yorders[32];};
+struct orders{int Xorders[32], Yorders[32], packageAmount;};
 struct orders o1;
-
-//struct amount{int amountOfZero, amountOfOne, amountOfTwo, amountOfThree;};
 
 void stockroomRoutine();
 void nextRound();
