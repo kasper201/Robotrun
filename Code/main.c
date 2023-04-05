fd7b68254d3c780d77cee6a1349098aa1a3a58ae
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-05 13:10:47 +0200

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -19,101 +19,91 @@ enum STATE cSTATE = init;
 
 int main()
 {
-	int percentage = 100;
-
-	switch(cSTATE)  // alle cSTATE staan er al maar de rest van je case dien je zelf nog aan te vullen en je zult if/else statments moeten gaan maken voor de cSTATE
+	while(1)
 	{
-		case init: //init state
-		clear();
-		initRobot();
-		ComCheck();
-		followLine(0,0);
-		ComCheck();
-		cSTATE = home;
-		break;
+		int percentage = 60;
+		o1.packageAmount = 2;
+		int backOrNot;
 		
-		
-		case maze: //maze state
-		;int backOrNot;
-		ComCheck();
-		solveMaze(&backOrNot);
-		ComCheck();
-		if(backOrNot == 1)
+		switch(cSTATE)  // alle cSTATE staan er al maar de rest van je case dien je zelf nog aan te vullen en je zult if/else statments moeten gaan maken voor de cSTATE
 		{
-			cSTATE = stockroom;
-		}
-		else if(backOrNot == 0)
-		{
+			case init: //init state
+			clear();
+			initRobot();
+			followLine(0,0);
+			set_motors(0,0);
 			cSTATE = home;
-		}
-		break;
-		
-		
-		case stockroom: //stockroom state
-		ComCheck();
-		stockroomRoutine();
-		ComCheck();
-		set_motors(50, 50);
-		delay_ms(100);
-		cSTATE = maze;
-		break;
-		
-		
-		case charge: //charge state
-		ComCheck();
-		solveMaze(0);
-		ComCheck();
-		int *ChargePoint = 0;
-		passingToCharge();
-		ComCheck();
-		followCharge(ChargePoint);
-		ComCheck();
-		timeToCharge();
-		ComCheck();
-		passingToCharge2();
-		ComCheck();
-		solveMaze(0);
-		cSTATE = home;
-		break;
-		
-		
-		case manual: //manual state
-		floefsWakes();
-		cSTATE = init;
-		break;
-		
-		
-		case home: //home state
-		clear();
-		ComCheck();
-		print("FLOEFS");
-		lcd_goto_xy(0,1);
-		print("WAKES");
-		play("o5 c#" );
-		delay_ms(1000);
-		if(percentage<60)
-		{
-			cSTATE = charge;
-		}
-		if(o1.packageAmount>0)
-		{
+			break;
+			
+			
+			case maze: //maze state
+			solveMaze(&backOrNot);
+			if(backOrNot == 1)
+			{
+				cSTATE = stockroom;
+			}
+			else if(backOrNot == 0)
+			{
+				cSTATE = home;
+			}
+			break;
+			
+			
+			case stockroom: //stockroom state
+			stockroomRoutine();
+			delay_ms(100);
 			cSTATE = maze;
+			break;
+			
+			
+			case charge: //charge state
+			solveMaze(0);
+			int *ChargePoint = 0;
+			passingToCharge();
+			followCharge(ChargePoint);
+			timeToCharge();
+			passingToCharge2();
+			solveMaze(0);
+			cSTATE = home;
+			break;
+			
+			
+			case manual: //manual state
+			//floefsWakes();
+			cSTATE = init;
+			break;
+			
+			
+			case home: //home state
+			clear();
+			print("FLOEFS");
+			lcd_goto_xy(0,1);
+			print("WAKES");
+			delay_ms(1000);
+			if(percentage<60)
+			{
+				cSTATE = charge;
+			}
+			if(o1.packageAmount>0)
+			{
+				cSTATE = maze;
+			}
+			break;
+			
+			
+			case lost: //lost state
+			clear();
+			print("HELP|:(");
+			lcd_goto_xy(0,1);
+			print("FLOEFS");
+			play("o5 c#" );
+			delay_ms(300);
+			if(1==2)
+			{
+				cSTATE = manual;
+			}
+			break;
 		}
-		break;
-		
-		
-		case lost: //lost state
-		clear();
-		print("HELP|:(");
-		lcd_goto_xy(0,1);
-		print("FLOEFS");
-		play("o5 c#" );
-		delay_ms(300);
-		if(1==2)
-		{
-			cSTATE = manual;
-		}
-		break;
 	}
-return 0;
+	return 0;
 }
