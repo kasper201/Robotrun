b96c5e32ed77fd4149d9c0edb1b23fb733068141
Flits <kasperjnssen@gmail.com>
2023-04-05 12:59:28 +0200

    Update MazeSolve.c

--- Code/SolveMaze/MazeSolve.c
+++ Code/SolveMaze/MazeSolve.c
@@ -1,3 +1,130 @@
+/* Robotrun - an application for the Pololu 3pi Robot
+ *
+ * This application uses the Pololu AVR C/C++ Library.  For help, see:
+ * -User's guide: http://www.pololu.com/docs/0J20
+ * -Command reference: http://www.pololu.com/docs/0J18
+ *
+ * Created: 3/18/2023 11:16:12 AM
+ *  Author: Kasper
+ */
+
+/*
+ * Part of this code was derived but not copied from Pololu at https://www.pololu.com/docs/0J21/8.e
+ * Though this isn't a copy or just plain rewritten in different variable names large parts do look alike
+ */
+
+#include <pololu/3pi.h>
+#include "FindLine.h"
+#include "MazeSolve.h"
+
+char arrayToStockroom[128] = {0};
+char arrayFromStockroom[128] = {0};
+
+
+void toStockRoom()
+{
+	int i = 0;
+	int check = 0;
+	while(1)
+	{
+		followLine(&check,1);
+		turn(arrayToStockroom[i]);
+		if(check == 7)
+		{
+			break;
+		}
+		else if(check == 99)
+		{
+			break;
+		}
+		i++;
+	}
+}
+
+void fromStockRoom()
+{
+	int i = 0;
+	int check = 0;
+	while(1)
+	{
+		followLine(&check,1);
+		turn(arrayFromStockroom[i]);
+		if(check == 7)
+		{
+			break;
+		}
+		else if(check == 99)
+		{
+			break;
+		}
+		i++;
+	}
+}
+
+void routeKnown(int *ToStockRoom)
+{	
+	if(*ToStockRoom)
+	{
+		toStockRoom();
+		*ToStockRoom = 0;
+	}
+	else
+	{
+		fromStockRoom();
+		*ToStockRoom = 1;
+	}
+}
+
+void convertArray(int arrayLength)
+{
+	for(int i = arrayLength; i > 0; i--)
+	{
+		if(arrayToStockroom[i] == 3)
+		{
+			arrayFromStockroom[arrayLength-i] = 1;
+		}
+		else if(arrayToStockroom[i] == 1)
+		{
+			arrayFromStockroom[arrayLength-i] = 3;
+		}
+		else
+		{
+			arrayFromStockroom[i] = 0;
+		}
+	}
+}
+
+void simplify(int crossing, int *pathLength)
+{
+	if(*pathLength < 3 || arrayToStockroom[(*pathLength-2)] != 2)
+		return;
+		
+	int i;
+	int totalTurn = 0;
+	for(i = 1; i <= 3; i++)
+	{
+		switch(arrayToStockroom[*pathLength-i])
+		{			
+			case 1:
+			totalTurn += 1;
+			break;
+			
+			case 3:
+			totalTurn += 3;
+			break;
+			
+			case 2:
+			totalTurn += 2;
+			break;
+		}
+	}
+	totalTurn %= 4;
+	
+	arrayToStockroom[*pathLength-3] = totalTurn;
+	
+	*pathLength -= 2;
+}
+
 void solveMaze(int *mazeLocation)
 {
 	*mazeLocation %= 2;
