3dd08765ad7911a0f2aa672c44c89c78069ee66f
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-05 14:25:38 +0200

    Update charge.h

--- Code/charging/charge.h
+++ Code/charging/charge.h
@@ -1,10 +1,11 @@
 #ifndef Charging_H_
 #define Charging_H_
 
+void chargeRoutine();
 void passingToCharge();
 void passingToCharge2();
 void batteryLevel();
 void timeToCharge();
-void batteryRead( unsigned int *percentage)
+void batteryRead( unsigned int *percentage);
 
 #endif
