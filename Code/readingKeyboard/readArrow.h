ccb561bea2befa74b6347cdbc5745137f9d4328e
Flits <kasperjnssen@gmail.com>
2023-04-12 11:06:57 +0200

    Delete Code/readingKeyboard directory

--- Code/readingKeyboard/readArrow.h
+++
@@ -1,8 +0,0 @@
-#ifndef READARROW_H_INCLUDED
-#define READARROW_H_INCLUDED
-
-int readArrow(void);
-
-#endif READARROW_H_INCLUDED
-
-
--- Code/readingKeyboard/readKeys.h
+++
@@ -1,6 +0,0 @@
-#ifndef READKEYS_H_INCLUDED
-#define READKEYS_H_INCLUDED
-
-int readKeys(void);
-
-#endif // READKEYS_H_INCLUDED
