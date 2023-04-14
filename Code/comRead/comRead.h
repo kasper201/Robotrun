b9cff1dc19e665e2613a6386384ebf791680132e
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-14 13:00:32 +0200

    Update comRead.h

--- Code/comRead/comRead.h
+++ Code/comRead/comRead.h
@@ -1,7 +1,9 @@
-#ifndef ORDERRECIEVE_H_
-#define ORDERRECIEVE_H_
+#ifndef ComRead_H_
+#define ComRead_H_
 
 void receiveOrderToBreak();
 void receiveOrder();
+char readNextByte();
+void batteryRead(unsigned int *percentage);
 
 #endif
