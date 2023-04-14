fe8e198ff1dcb6685ed9e5e78f71b3ff6644666e
mikerino0 <126207852+mikerino0@users.noreply.github.com>
2023-04-14 11:27:08 +0200

    Update stockroom.h

--- Code/stockroom/stockroom.h
+++ Code/stockroom/stockroom.h
@@ -4,17 +4,16 @@
 enum direction{minX, plusX, minY, plusY};
 enum direction facing;
 
-struct coords{int Xcurrent, Ycurrent, Xpackage, amountOfX;};
+struct coords{int Xcurrent, Ycurrent, Xpackage, Ypackage;};
 struct coords p1;
 
 struct orders{int Xorders[32], Yorders[32], packageAmount;};
 struct orders o1;
 
 void stockroomRoutine();
-void nextRound();
-void findPackageX();
-void findPackageY();
+void findPackage();
 void TurnBack();
 void turnToY();
+void turnToX();
 
 #endif
